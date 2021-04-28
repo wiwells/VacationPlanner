897c8c0fa909778f8e3fed67951c4f17e375e58b
vnm303 <79052072+vnm303@users.noreply.github.com>
2021-04-28 15:50:52 -0500

    Add files via upload

--- src/application/VPController.java
+++ src/application/VPController.java
@@ -13,6 +13,11 @@ package application;
 import application.model.MVCPlanner;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 public class VPController {
@@ -24,6 +29,22 @@ public class VPController {
 
     @FXML
     void location(ActionEvent event) {
+    	try {
+            AnchorPane homeParent = (AnchorPane) FXMLLoader.load(getClass().getResource("Location.fxml"));
+            Scene homeScene = new Scene(homeParent, 750, 750);
+            homeScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            //Get stage info
+            Stage window = (Stage)((Node) event.getSource()).getScene().getWindow();
+            window.setScene(homeScene);
+            window.setResizable(false);
+            //set the title 
+            window.setTitle("Location");
+            //display the scene
+            window.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
